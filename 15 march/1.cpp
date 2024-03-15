a92dbc5a2a25800a075c17b20e3cd78ac507e9d7
Tony Ganesh Prajapati <133465084+gittonyp@users.noreply.github.com>
2024-03-15 04:31:02 +0000

    dsf

---
+++ 15 march/1.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void facti(){
+int a;
+scanf("%d",&a);
+int z=1;
+for (int i = 1; i < a; i++)
+{
+    z=i*z;
+}
+printf("%d",z);
+printf("\n");
+return;
+}
+int main(){
+    facti();
+}
+
